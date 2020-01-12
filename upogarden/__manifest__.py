3de0edc42069a78df538fd41f8d1cdd735e82ce2
Juanorma16 <juanorma2h@gmail.com>
2020-01-12 14:12:33 +0100

    Subida con datos ejemplo

--- upogarden/__manifest__.py
+++ upogarden/__manifest__.py
@@ -8,6 +8,6 @@
     'version': '0.1',
     'depends': ['base'],
     'data': ['views/cliente_view.xml','views/producto_view.xml','views/alquiler_view.xml','views/contratacion_view.xml','views/servicio_view.xml','views/proveedor_view.xml','views/entrega_view.xml','views/presupuesto_view.xml','views/trabajador_view.xml'],
-    'demo': [ ],
+    'demo': ['demo/demo.xml'],
     'application': True,
 }
